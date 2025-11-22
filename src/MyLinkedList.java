f5f5d847674842ed1043224ac8c7b8da1fd9c486
Pankaj Thakur <pankaj@consciousitservice.ca>
2025-11-21 19:55:10 -0800

    Implemented Feature 1: Add and Print

---
+++ src/MyLinkedList.java
@@ -0,0 +1,48 @@
+public class MyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    // FEATURE 1: Add node
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    // FEATURE 1: Print list
+    public void printList() {
+        Node current = head;
+        System.out.print("List: ");
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+
+        System.out.println("--- Testing Feature 1: Add & Print ---");
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.printList();
+    }
+}
